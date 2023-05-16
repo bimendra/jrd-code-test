445eeda817c571972df598202a1cce4ad7fc0945
bimendra <bimendra.85@gmail.com>
2023-05-16 16:32:29 +0800

    Import vehicle component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import Vehicle from './lib/interfaces/vehicle.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'jrd-code-test';
+  vehicle: Vehicle = {
+    name: 'Hatchback',
+    maxPassengers: 2,
+    thumbnailUrl:
+      'https://res.jayride.com/web/dotcom/vehicle/370x300/car-economy-sedan.png',
+  };
 }
