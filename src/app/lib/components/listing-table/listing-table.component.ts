04c7641fc95cc9c8bcbd9a40b2b416d6f672e2c5
bimendra <bimendra.85@gmail.com>
2023-05-17 14:09:14 +0800

    Refactor - delegate up data fetching to the page level

--- src/app/lib/components/listing-table/listing-table.component.ts
+++ src/app/lib/components/listing-table/listing-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { QuoteRequestService } from 'src/app/quote-request.service';
 import QuoteRequest from '../../interfaces/quote-request.interface';
 import Listing from '../../interfaces/listing.interface';
@@ -11,67 +11,5 @@ import { orderBy } from 'lodash';
   styleUrls: ['./listing-table.component.scss'],
 })
 export class ListingTableComponent {
-  constructor(private quoteRequestService: QuoteRequestService) {
-    this.refreshListings();
-    this.sortListings = this.sortListings.bind(this);
-    this.filterListingsByPassengerCount =
-      this.filterListingsByPassengerCount.bind(this);
-  }
-
-  quoteRequest?: QuoteRequest;
-
-  page: number = 1;
-  pageSize: number = 6;
-  listings: Listing[] = [];
-  collectionSize: number = this.listings.length;
-  displayedListings: Listing[] = [];
-
-  maxPassengers = 3;
-  orderCriteria: string = 'pricePerPassenger';
-  sortOrder: 'asc' | 'desc' = 'asc';
-
-  ngOnInit() {
-    this.getQuoteRequest();
-  }
-
-  getQuoteRequest() {
-    this.quoteRequestService
-      .getQuoteRequest()
-      .pipe(
-        map((quoteRequest) => {
-          quoteRequest.listings = this.sortListings(quoteRequest.listings);
-          return quoteRequest;
-        })
-      )
-      .subscribe((quoteRequest) => {
-        this.quoteRequest = quoteRequest;
-        this.listings = this.quoteRequest.listings;
-        this.refreshListings();
-        this.collectionSize = this.listings.length;
-      });
-  }
-
-  refreshListings() {
-    this.displayedListings = this.filterListingsByPassengerCount(
-      this.sortListings(this.listings)
-    )
-      .map((listing, i) => ({
-        id: i + 1,
-        ...listing,
-      }))
-      .slice(
-        (this.page - 1) * this.pageSize,
-        (this.page - 1) * this.pageSize + this.pageSize
-      );
-  }
-
-  sortListings(listings: Listing[]) {
-    return orderBy(listings, this.orderCriteria, this.sortOrder);
-  }
-
-  filterListingsByPassengerCount(listings: Listing[]) {
-    return listings.filter(
-      (listing) => listing.vehicleType.maxPassengers >= this.maxPassengers
-    );
-  }
+  @Input() listings?: Listing[];
 }
