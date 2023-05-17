6af95a9630ff73d64365b10642352aa13bd44914
bimendra <bimendra.85@gmail.com>
2023-05-17 20:55:08 +0800

    Add theme switcher

--- src/app/lib/components/sidebar/sidebar.component.ts
+++ src/app/lib/components/sidebar/sidebar.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jrd-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
+  theme: string = 'light';
 
+  onCheckboxChange(e: any) {
+    this.theme = this.theme === 'dark' ? 'light' : 'dark';
+    document.body.setAttribute('data-bs-theme', this.theme);
+  }
 }
