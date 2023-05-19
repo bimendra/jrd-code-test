e7701e46cc05fbe41de588d8b107ac153a6ba82e
bimendra <bimendra.85@gmail.com>
2023-05-19 19:06:51 +0800

    Add server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "/dist/jrd-code-test")));
+
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/dist/jrd-code-test/index.html"))
+);
+
+app.listen(3000);
